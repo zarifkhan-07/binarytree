c4511dabd337795888f68c97cb3acd184c2786db
zarifkhan-07 <zarifsuhana1@gmail.com>
2026-05-15 21:44:28 +0600

    Add files via upload

---
+++ binarytree postorder.py
@@ -0,0 +1,31 @@
+from binarytree import build
+
+class Node:
+    def __init__(self,key):
+        self.right=None
+        self.left=None
+        self.val=key
+    
+def print_postorder(root):
+    if root:
+        print_postorder(root.left)
+
+        print(root.val, end="")
+
+        print_postorder(root.right)
+    
+
+if __name__ == '__main__':
+    root=Node(1)
+    root.left=Node(2)
+    root.right=Node(3)
+    root.left.left=Node(4)
+    root.left.right=Node(5)
+
+tree_list=[1,2,3,4,5]
+visual_tree=build(tree_list)
+print("\nBinary tree visualization")
+print(visual_tree)
+print("Postorder traversal of binary tree is:")
+print_Postorder(root)
+print()
